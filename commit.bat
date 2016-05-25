6d823fc5148d706e52e638dbac319110beee4652
Engyrus (Jerry Kindall) <engyrus@gmail.com>
2016-05-24 17:18:27 -0700

    strip quote marks from commit message due to batch file shenanigans

--- commit.bat
+++ commit.bat
@@ -2,9 +2,7 @@
 set secs=2
 git status
 set /p msg=Commit message: 
-setlocal enabledelayedexpansion
-set msg=!msg:"=!
-echo %msg%
+set msg=%msg:"=%
 git add --all
-git commit --message "%msg%" || set secs=60
+git commit --message "!msg!" || set secs=60
 timeout /t %secs%
