3893a068b3255a043fef9368e167de0a7efbf89a
Engyrus (Jerry Kindall) <engyrus@gmail.com>
2016-05-25 14:27:37 -0700

    tweaks to commit batch script

--- commit.bat
+++ commit.bat
@@ -1,8 +1,8 @@
 @echo off
-set secs=2
+set secs=1
 git status
 set /p msg=Commit message: 
 set msg=%msg:"=%
 git add --all
-git commit --message "!msg!" || set secs=60
+git commit --message "%msg%" || set secs=60
 timeout /t %secs%
